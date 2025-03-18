b54e7ce6e1fdc55e1b27974b6db5549649178108
Ibrahim Lawali <professe84@gmail.com>
2025-03-18 04:42:08 +0100

    Update Vite server port to 3003 for configuration consistency

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
-    port: 3000, // Spécifiez un port différent ici
+    port: 3003, // Specify a different port
   },
   resolve: {
     alias: {
